5a1b92195d0243df22b37893127c1c68d1d75acb
antoniooreany <4531858+antoniooreany@users.noreply.github.com>
2025-02-20 18:37:37 +0100

    Refactor book routes and update dependencies in requirements.txt

--- app.py
+++ app.py
@@ -9,5 +9,3 @@ app.config['SECRET_KEY'] = 'a73335abd8e3a7683de8a98a2ece9581'  # Replace with a 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Import models after db is defined
-from app.models import User, Book

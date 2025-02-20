89ad239ffef9dae2d7d721a9b0d831250649e260
antoniooreany <4531858+antoniooreany@users.noreply.github.com>
2025-02-20 20:20:13 +0100

    Fix comment in user creation logic in auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -32,7 +32,7 @@ def signup():
         username = request.form['username']
         password = request.form['password']
         hashed_password = generate_password_hash(password, method='sha256')
-        new_user = User(username=username, password=hashed_password)
+        new_user = User(username=username, password=hashed_password) # todo why asks to remove both parameters?
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('auth.login'))

7c3c3778c2609f75faa2163b33e53dfedaf421a9
antoniooreany <4531858+antoniooreany@users.noreply.github.com>
2025-02-20 19:06:11 +0100

    Refactor __init__.py to remove commented code and enhance index.html with a complete structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,37 +1,3 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-#
-# db = SQLAlchemy()
-# migrate = Migrate()
-#
-# # def create_app():
-# #     app = Flask(__name__)
-# #     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-# #     app.config['SECRET_KEY'] = 'a73335abd8e3a7683de8a98a2ece9581'
-# #
-# #     db.init_app(app)
-# #     migrate.init_app(app, db)
-# #
-# #     from app.routes.books import books_bp
-# #     app.register_blueprint(books_bp)
-# #
-# #     return app
-#
-#
-# def create_app():
-#     app = Flask(__name__)
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-#     app.config['SECRET_KEY'] = 'a73335abd8e3a7683de8a98a2ece9581'
-#
-#     db.init_app(app)
-#     migrate.init_app(app, db)
-#
-#     from app.routes.books import books_bp
-#     app.register_blueprint(books_bp)
-#
-#     return app
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
